95942b9122d20b52fbb371f0582200d3ddfc2a8c
Ersin Köseoğlu <63159627+Ersinkos@users.noreply.github.com>
2023-12-14 20:05:45 +0300

    level design for test

---
+++ 2D_PlatformerCS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	private Vector3 offset = new Vector3(0f, 3f, -10f);
+	private float smoothFactor = 10f;
+	private Vector3 velocity = Vector3.zero;
+	[SerializeField] Transform target;
+
+	private void LateUpdate()
+	{
+		Follow();
+	}
+	public void Follow()
+	{
+		Vector3 targetPosition = target.position + offset;
+		Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor);
+
+		transform.position = smoothPosition;
+	}
+}
